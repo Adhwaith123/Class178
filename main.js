00e96d2a170fc1916ccef0e8fd08ded4dbb0476f
Adhwaith123 <62431227+Adhwaith123@users.noreply.github.com>
2022-06-07 17:50:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,12 +7,47 @@ var map=new mapboxgl.Map({
     center:[longtitude,latitude],
     zoom:16
     
-})
+});
 map.addControl(
-    new mapboxgl.GeolocateControl({
-        positionOptions:{
-            enableHighAccuracy:true
-        },
-        trackUserLocation:true
-    })
-)
+    new MapboxDirections({
+        accessToken:mapboxgl.accessToken
+    }),'top-left'
+    
+);
+map.addControl(
+     new mapboxgl.GeolocateControl({
+            positionOptions:{
+                enableHighAccuracy:true
+            },
+            trackUserLocation:true
+        })
+);
+// let latitude=22.7868542, longitude=88.3643296;
+
+// // Initializing Mapbox
+// mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+
+// var map = new mapboxgl.Map({
+// 	container: 'map',
+// 	style: 'mapbox://styles/mapbox/streets-v11',
+// 	center: [longitude, latitude],
+// 	zoom: 16
+// });   
+
+// map.addControl(
+// 	new mapboxgl.GeolocateControl({
+// 		positionOptions: {
+// 			enableHighAccuracy: true
+// 		},
+// 		trackUserLocation: true
+// 	})
+// );
+
+// map.addControl(
+// 	new MapboxDirections({
+// 		accessToken: mapboxgl.accessToken
+// 	}),
+// 	'top-left'
+// );
+
+   
